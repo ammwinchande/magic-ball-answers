ef4e068a4d54647f15a7e8895ce8c991a9743bbe
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2019-10-06 02:00:48 +0300

    added appbar and required colors for AppBar and Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: BallGame(),
       ),
-    );
+    ),
+  );
+}
+
+class BallGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
